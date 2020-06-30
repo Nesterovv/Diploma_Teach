8117dd72a6c49d226f086cd2408883c153a61787
Anton Nesterov <56961548+Nesterovv@users.noreply.github.com>
2020-06-30 03:16:27 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { Filmitem } from './components/Filmitem';
+import { Common } from './components/Common';
+
+export function App() {
+  return (
+    <div>
+      <Common />
+    </div>
+  );
+}
+export default (App);
